304acbb5400da51299de093100d2f1199502d11a
S-Cesc <cesc.sasal@gmail.com>
2024-11-07 12:51:24 +0100

    merge

---
+++ prova_concepte/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/usr/lib/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/cesc/Documents/cursos/flutter/flutter_projecte_cifo/prova_concepte" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/usr/lib/flutter"
+  "PROJECT_DIR=/home/cesc/Documents/cursos/flutter/flutter_projecte_cifo/prova_concepte"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/cesc/Documents/cursos/flutter/flutter_projecte_cifo/prova_concepte/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/cesc/Documents/cursos/flutter/flutter_projecte_cifo/prova_concepte/lib/main.dart"
+)
---
+++ prova_concepte_2/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/usr/lib/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/cesc/Documents/cursos/flutter/flutter_projecte_cifo/prova_concepte_2" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/usr/lib/flutter"
+  "PROJECT_DIR=/home/cesc/Documents/cursos/flutter/flutter_projecte_cifo/prova_concepte_2"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/cesc/Documents/cursos/flutter/flutter_projecte_cifo/prova_concepte_2/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/cesc/Documents/cursos/flutter/flutter_projecte_cifo/prova_concepte_2/lib/main.dart"
+)
